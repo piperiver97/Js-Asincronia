93922979d390a92072286a404fa42f9481388535
piperiver97 <feliperiveros_123@gmail.com>
2024-07-05 23:25:02 +0200

    finish

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-np
+const data = {
+    "results": [
+      {
+        "title": "Estudiar Js",
+        "priority": "alta",
+        "isDone": false
+      },
+      {
+        "title": "Estudiar CSS",
+        "priority": "alta",
+        "isDone": true
+      },
+      {
+        "title": "Estudiar OOP",
+        "priority": "media",
+        "isDone": false
+      },
+      {
+        "title": "Estudiar IA",
+        "priority": "baja",
+        "isDone": false
+      }
+    ]
+  };
+  
+  data.results.forEach(todo => {
+    console.log(todo.title);
+  });
+  
+
+    const tableBody = document.getElementById('todos-table-body');
+
+    data.results.forEach(todo => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${todo.title}</td>
+            <td>${todo.priority}</td>
+            <td>${todo.isDone ? 'Sí' : 'No'}</td>
+        `;
+        tableBody.appendChild(row);
+    });
